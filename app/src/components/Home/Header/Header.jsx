1996238be48e23ae05901aaeb42abd6fd6c3790d
OumouDi <124316387+OumouDi@users.noreply.github.com>
2023-11-14 14:42:19 +0100

    header

--- app/src/components/Home/Header/Header.jsx
+++ app/src/components/Home/Header/Header.jsx
@@ -1,21 +1,31 @@
-
-import React from 'react';
-import './header.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./header.css";
+import { Link } from "react-router-dom";
+import { BsGithub } from "react-icons/bs";
+import { SiLinkedin } from "react-icons/si";
 
 const Header = () => {
   return (
-    <div className='header-container'>
-        <nav className='nav'>
-            <ul>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/project'>Project</Link></li>
-                <li><Link to='/about'>About</Link></li>
-      
-            </ul>
-        </nav>
+    <div className="header-container">
+      <nav className="nav">
+        <ul>
+          <li>
+            <Link to="/" className="active">Home</Link>
+          </li>
+          <li>
+            <Link to="/project">Project</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="social-media">
+        <Link to="https://github.com/OumouDi/"><BsGithub  className="social-icon"/></Link>
+        <Link to="https://www.linkedin.com/in/oumou-diakhaby-9a7941199/"><SiLinkedin className="social-icon" /></Link>
+        </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
