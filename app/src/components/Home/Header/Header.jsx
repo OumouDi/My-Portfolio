e1559a3915ac6425047e69b769c28319ff8e9509
OumouDi <124316387+OumouDi@users.noreply.github.com>
2023-12-10 15:21:31 +0100

    responsive navbar

--- app/src/components/Home/Header/Header.jsx
+++ app/src/components/Home/Header/Header.jsx
@@ -4,70 +4,90 @@ import { Link } from "react-router-dom";
 import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
 import { BsGithub } from "react-icons/bs";
 import { SiLinkedin } from "react-icons/si";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
     <div className="header-container">
-      <nav className="nav">
-        <ul>
-          <li>
-            <Link activeClass="active"> Home </Link>
-          </li>
-          <li>
-            <ScrollLink
-              activeClass="active"
-              to="project"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-            >
-              Projects
-            </ScrollLink>
-          </li>
-          <li>
-            <ScrollLink
-              activeClass="active"
-              to="skills"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-            >
-              Skills
-            </ScrollLink>
-          </li>
-          <li>
-            <ScrollLink
-              activeClass="active"
-              to="about"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-            >
-              About
-            </ScrollLink>
-          </li>
-          <li>
-            <ScrollLink
-              activeClass="active"
-              to="contact"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-            >
-              Contact
-            </ScrollLink>
-          </li>
-        </ul>
-      </nav>
+      <div className="burger-icon" onClick={toggleNav}>
+        {isNavOpen ? <FaTimes /> : <FaBars />}
+      </div>
+      <nav className={`nav ${isNavOpen ? "active" : ""}`}>
+          <ul>
+            <li>
+              <Link activeClass="active"> Home </Link>
+            </li>
+            <li>
+              <ScrollLink
+                activeClass="active"
+                to="project"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                Projects
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink
+                activeClass="active"
+                to="skills"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                Skills
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink
+                activeClass="active"
+                to="about"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                About
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink
+                activeClass="active"
+                to="contact"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                Contact
+              </ScrollLink>
+            </li>
+          </ul>
+        </nav>
+
+
       <div className="social-media">
-        <a href="https://github.com/OumouDi/" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://github.com/OumouDi/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <BsGithub className="social-icon" />
         </a>
-        <a href="https://www.linkedin.com/in/oumou-diakhaby-9a7941199/" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.linkedin.com/in/oumou-diakhaby-9a7941199/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <SiLinkedin className="social-icon" />
         </a>
       </div>
